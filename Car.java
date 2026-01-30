1876169836a567bc6d0c90f7df8f0defe866d5eb
infocareerdemo <infocareerdemo@gmail.com>
2026-01-30 08:04:55 +0530

    Datatypes and Variables

---
+++ Car.java
@@ -0,0 +1,21 @@
+class Car
+{
+public static void main(String args[])
+{ 
+String carName="Tesla Model S";
+
+String mileage="396 miles";
+
+double price=85000.00d;
+
+String brand="Tesla";
+String fuelType="Electric";
+System.out.println("Car Name :"+carName);
+System.out.println("Brand :"+brand);
+
+
+System.out.println("Fuel Type :"+fuelType);
+System.out.println("Mileage :"+mileage);
+System.out.println("Price :"+price);
+}
+}
---
+++ Food.java
@@ -0,0 +1,19 @@
+class Food
+{
+public static void main(String args[])
+{ 
+String foodName="Paneer Butter Masala";
+
+int calories=400;
+
+float price=250.50f;
+
+boolean isVegetarian=true;
+System.out.println("foodName :"+foodName);
+System.out.println("Cuisine Type :Indian");
+
+
+System.out.println("price :"+price);
+System.out.println("Vegetarian :"+isVegetarian);
+}
+}
